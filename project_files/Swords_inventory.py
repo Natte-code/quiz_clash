810079867e0d72305355a11078f90f39c880770e
Pilou0715 <eliot.pangrazzi@elev.ga.ntig.se>
2025-01-21 20:28:54 +0100

    Försök av inventory system
    Försökt av att göra ett enkelt inventory system för potion

--- project_files/Swords_inventory.py
+++ project_files/Swords_inventory.py
@@ -67,3 +67,42 @@ print(player.inventory)
 #Svärden behöver bara hanteras som föremål allt annat kan vi göra som en siffra i en variabel
 #så tex när den ska dra en potion så drar den och bara ändrar Potion = 2 tex istället för att ha det som ett föremål så får combat systemet hantera healing systemet
 #samma med totem systemet.
+
+
+
+#min förösk här 
+antal_potion_vanlig = 0
+antal_potion_epic = 0
+
+print('''
+1. Vissa inventory
+2. Lägg till potion vanlig
+3. Lägg till potion epic
+4. Tar bort potion  vanlig
+5. Tar bort potion epic  
+       ''')
+fraga_potion_inventory = str(input())
+
+if fraga_potion_inventory == "1":
+    print (f'Du har {antal_potion_vanlig} vanliga potion')
+    print (f"Du har {antal_potion_epic} epic potion")
+
+elif fraga_potion_inventory == "2":
+    antal_potion_vanlig = antal_potion_vanlig + 1
+    print (f'Du har nu {antal_potion_vanlig} vanliga potion')
+
+elif fraga_potion_inventory == "3":
+    antal_potion_epic = antal_potion_epic + 1
+    print (f'Du har nu {antal_potion_epic} epic potion')
+
+elif fraga_potion_inventory == "4":
+    antal_potion_vanlig = antal_potion_vanlig - 1
+    print (f'Du har nu {antal_potion_vanlig} vanliga potion')
+
+
+elif fraga_potion_inventory == "5":
+    antal_potion_epic = antal_potion_epic - 1
+    print (f'Du har nu {antal_potion_epic} epic potion')
+
+else:
+    print ("Ogiltlig svar")
